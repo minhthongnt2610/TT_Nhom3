938c0b778a9275c46dc0f759cbe31dc234ed509b
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-24 22:05:51 +0700

    tạo màu background cho màn hình và cập nhật màu cho app_colors.dart

--- TimeTrack/lib/contains/app_colors.dart
+++ TimeTrack/lib/contains/app_colors.dart
@@ -1,3 +1,6 @@
-class AppColors{
+import 'dart:ui';
 
+class AppColors {
+  static const hexFFF0F0 = Color(0xFFFFF0F0);
+  static const hexD79E4E = Color(0xFFD79E4E);
 }
--- TimeTrack/lib/screens/common_screens/login_screen.dart
+++ TimeTrack/lib/screens/common_screens/login_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:timetrack/contains/app_colors.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [AppColors.hexFFF0F0, AppColors.hexD79E4E],
+            ),
+          ),
+          child: Container(),
+        ),
+      ),
+    );
   }
 }
