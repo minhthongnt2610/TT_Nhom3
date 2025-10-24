2fd7ba2fe31807e4e36c42c45a3b5e8b937199c3
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-24 21:54:30 +0700

    tạo màn hình login_screen.dart và cài đặt my_app.dart

--- TimeTrack/lib/my_app.dart
+++ TimeTrack/lib/my_app.dart
@@ -4,6 +4,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Time Track",
+
+    );
   }
 }
