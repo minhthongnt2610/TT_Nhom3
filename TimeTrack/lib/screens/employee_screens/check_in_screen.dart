8e205dad56ac7a529268ee7e3aefc602ab718b95
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-26 22:38:53 +0700

    cập nhật thành công clock_widget.dart

--- TimeTrack/lib/screens/employee_screens/check_in_screen.dart
+++ TimeTrack/lib/screens/employee_screens/check_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:timetrack/screens/employee_screens/widgets/app_bar_widget.dart';
+import 'package:timetrack/screens/employee_screens/widgets/clock_widget.dart';
 import 'package:timetrack/screens/employee_screens/widgets/event_button.dart';
 
 import '../../contains/app_colors.dart';
@@ -12,6 +13,29 @@ class CheckInScreen extends StatefulWidget {
 }
 
 class _CheckInScreenState extends State<CheckInScreen> {
+  ClockWidget clockWidget = ClockWidget();
+  late String _currentTime;
+  late String _currentDate;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _currentTime = clockWidget.getCurrentTime();
+    _currentDate = clockWidget.getCurrentDate();
+    clockWidget.startTime(() {
+      setState(() {
+        _currentTime = clockWidget.getCurrentTime();
+        _currentDate = clockWidget.getCurrentDate();
+      });
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    clockWidget.timer.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     int height = MediaQuery.of(context).size.height.toInt();
@@ -29,63 +53,82 @@ class _CheckInScreenState extends State<CheckInScreen> {
             colors: [AppColors.hexFFF0F0, AppColors.hexD42C42],
           ),
         ),
-        child: Padding(
-          padding: EdgeInsets.only(top: 241 * height / 956),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  EventButton(
-                    onTap: () {},
-                    urlImage: "assets/logo/logo.png",
-                    text: "Đơn xin chấm\ncông bổ sung",
-                  ),
-                  EventButton(
-                    onTap: () {},
-                    urlImage: "assets/logo/logo.png",
-                    text: "Đơn xin\nnghỉ phép",
-                  ),
-                  EventButton(
-                    onTap: () {},
-                    urlImage: "assets/logo/logo.png",
-                    text: "Lịch sử\nchấm công",
-                  ),
-                  EventButton(
-                    onTap: () {},
-                    urlImage: "assets/logo/logo.png",
-                    text: "Trạng thái\n",
-                  ),
-                ],
-              ),
-              SizedBox(height: 46 * height / 956),
-              GestureDetector(
-                onTap: (){
-                  print("Check in");
-                },
-                child: Container(
-                  width: 223 * width / 440,
-                  height: 223 * height / 956,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [AppColors.hexFFFBFE, AppColors.hexFFE699],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 241 * height / 956),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    EventButton(
+                      onTap: () {},
+                      urlImage: "assets/logo/logo.png",
+                      text: "Đơn xin chấm\ncông bổ sung",
                     ),
-                    borderRadius: BorderRadius.circular(112 * height / 956),
-                    border: Border.all(
-                      color: AppColors.hexF8790A,
-                      width: 5 * height / 956,
+                    EventButton(
+                      onTap: () {},
+                      urlImage: "assets/logo/logo.png",
+                      text: "Đơn xin\nnghỉ phép",
                     ),
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "CHECK IN",
-                    style: TextStyle(fontSize: 40, color: AppColors.hexF8790A,),
+                    EventButton(
+                      onTap: () {},
+                      urlImage: "assets/logo/logo.png",
+                      text: "Lịch sử\nchấm công",
+                    ),
+                    EventButton(
+                      onTap: () {},
+                      urlImage: "assets/logo/logo.png",
+                      text: "Trạng thái\n",
+                    ),
+                  ],
+                ),
+                SizedBox(height: 46 * height / 956),
+                GestureDetector(
+                  onTap: () {
+                    print("Check in");
+                  },
+                  child: Container(
+                    width: 223 * width / 440,
+                    height: 223 * height / 956,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [AppColors.hexFFFBFE, AppColors.hexFFE699],
+                      ),
+                      borderRadius: BorderRadius.circular(112 * height / 956),
+                      border: Border.all(
+                        color: AppColors.hexF8790A,
+                        width: 5 * height / 956,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "CHECK IN",
+                      style: TextStyle(fontSize: 40, color: AppColors.hexF8790A),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 46 * height / 956),
+                Text(
+                  _currentTime,
+                  style: TextStyle(
+                    fontSize: 29 * height / 956,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                Text(
+                  _currentDate,
+                  style: TextStyle(
+                    fontSize: 20 * height / 956,
+                    color: Colors.black,
+                    fontFamily: 'tnr',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
