98988d4abe9247fe9940898a6e9c9ed4c3b58382
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-30 20:54:52 +0700

    hoàn thành màn hình thông tin users

--- TimeTrack/lib/screens/common_screens/widgets/build_info_field.dart
+++ TimeTrack/lib/screens/common_screens/widgets/build_info_field.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../contains/app_colors.dart';
+
 class BuildInfoField extends StatelessWidget {
   const BuildInfoField({super.key, required this.title});
   final String title;
 
   @override
   Widget build(BuildContext context) {
+    int height = MediaQuery.of(context).size.height.toInt();
+
     return Padding(
       padding: const EdgeInsets.only(bottom: 12),
       child: Stack(
@@ -22,10 +26,12 @@ class BuildInfoField extends StatelessWidget {
             child: Center(
               child: Text(
                 title,
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 12,
-                ),
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14 * height / 956,
+                  color: AppColors.hexD79E4E,
+                  fontFamily: 'balooPaaji',
+                )
               ),
             ),
           ),
--- TimeTrack/lib/screens/employee_screens/info_employee.dart
+++ TimeTrack/lib/screens/employee_screens/info_employee.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:timetrack/contains/app_colors.dart';
 
 import '../common_screens/widgets/build_info_field.dart';
 
@@ -8,9 +9,11 @@ class InfoEmployee extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String name = 'Phạm Lê Huyền Trân';
     int height = MediaQuery.of(context).size.height.toInt();
+    int width = MediaQuery.of(context).size.width.toInt();
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: AppColors.backgroundColor,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -25,61 +28,83 @@ class InfoEmployee extends StatelessWidget {
                 ),
               ),
 
-              const SizedBox(height: 10),
+              SizedBox(height: 10 * height / 956),
               const CircleAvatar(
                 radius: 60,
                 backgroundColor: Colors.black,
-                child: CircleAvatar(
-                  radius: 57,
-                  backgroundColor: Colors.white,
-                ),
+                child: CircleAvatar(radius: 57, backgroundColor: Colors.white),
               ),
 
-              const SizedBox(height: 12),
+              SizedBox(height: 12 * height / 956),
 
               Text(
-                'Nguyễn Minh Thông',
-                style: GoogleFonts.poppins(
-                  fontSize: 18,
+                name,
+                style: TextStyle(
                   fontWeight: FontWeight.w700,
-                  color: Colors.black,
-                ),
+                  fontSize: 28 * height / 956,
+                  color: AppColors.hexD79E4E,
+                  fontFamily: 'balooPaaji',
+                )
               ),
 
-              const SizedBox(height: 25),
+              SizedBox(height: 25 * height / 956),
 
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 30),
-                padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 25,
+                  horizontal: 15,
+                ),
                 decoration: BoxDecoration(
                   color: const Color(0xFFFDDDC6),
                   borderRadius: BorderRadius.circular(25),
                 ),
                 child: Column(
                   children: [
-                    BuildInfoField( title: 'Mã nhân viên',),
-                    BuildInfoField( title: 'CCCD',),
-                    BuildInfoField( title: 'Phòng ban',),
-                    BuildInfoField( title: 'Chức vụ',),
-                    BuildInfoField( title: 'Email',),
+                    BuildInfoField(title: 'Mã nhân viên'),
+                    BuildInfoField(title: 'CCCD'),
+                    BuildInfoField(title: 'Phòng ban'),
+                    BuildInfoField(title: 'Chức vụ'),
+                    BuildInfoField(title: 'Email'),
+                    GestureDetector(
+                      onTap: () {
+                        debugPrint("Đổi mật khẩu");
+                      },
+                      child: Padding(
+                        padding:  EdgeInsets.only(right: 8.0 * width / 440),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "Đổi mật khẩu",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 13 * height / 956,
+                              color: AppColors.hexD79E4E,
+                              fontFamily: 'balooPaaji',
+                            )
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
 
-              const SizedBox(height: 25),
+               SizedBox(height: 25 * height / 956),
 
-              // Nút đăng xuất
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFF4A261),
+                  backgroundColor: AppColors.hexF8790A,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  padding:
-                  const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 12,
+                  ),
                 ),
                 onPressed: () {
-                  // Xử lý đăng xuất
+                  debugPrint('Đăng xuất');
                 },
                 child: Text(
                   'Đăng xuất',
@@ -95,5 +120,4 @@ class InfoEmployee extends StatelessWidget {
       ),
     );
   }
-
 }
