f4219a4f5ed5c35b5f831975ade08de5c5edb215
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-11-28 10:25:19 +0700

    cập nhật

--- TimeTrack/lib/screens/employee_screens/attendance_history_screen/attendance_history_screen.dart
+++ TimeTrack/lib/screens/employee_screens/attendance_history_screen/attendance_history_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../../../contains/app_colors.dart';
 class AttendanceHistoryScreen extends StatefulWidget {
   const AttendanceHistoryScreen({super.key});
 
@@ -9,11 +11,24 @@ class AttendanceHistoryScreen extends StatefulWidget {
 class _AttendanceHistoryScreenState extends State<AttendanceHistoryScreen> {
   @override
   Widget build(BuildContext context) {
+    int height = MediaQuery.of(context).size.height.toInt();
+    int width = MediaQuery.of(context).size.width.toInt();
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor:Colors.transparent,
         elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [AppColors.hexFFF0F0, AppColors.hexD79E4E],
+          ),
+        ),
       ),
     );
   }
