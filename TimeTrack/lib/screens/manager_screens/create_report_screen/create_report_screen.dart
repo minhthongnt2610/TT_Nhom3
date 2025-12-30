3f3dd17bf1f95d25ed8d52e80b23e38aee643a5c
Trân Trân <shuyentran2205@gmail.com>
2025-12-30 20:10:18 +0700

    hoàn thành màn hình báo cáo

--- TimeTrack/lib/screens/manager_screens/create_report_screen/create_report_screen.dart
+++ TimeTrack/lib/screens/manager_screens/create_report_screen/create_report_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:timetrack/common_widget/button_admin.dart';
+import 'package:timetrack/screens/common_screens/document_screen/widgets/date_time_picker.dart';
 
 import '../../../contains/app_colors.dart';
 
@@ -10,10 +12,18 @@ class CreateReportScreen extends StatefulWidget {
 }
 
 class _CreateReportScreenState extends State<CreateReportScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController fromDate = TextEditingController();
+  final TextEditingController toDate = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    int height = MediaQuery.of(context).size.height.toInt();
+    int width = MediaQuery.of(context).size.width.toInt();
     return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor,
         centerTitle: true,
         title: const Text(
           "TẠO BÁO CÁO",
@@ -22,6 +32,27 @@ class _CreateReportScreenState extends State<CreateReportScreen> {
             fontSize: 20,
             fontWeight: FontWeight.bold,
             fontFamily: 'balooPaaji',
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50 * height / 956,
+                child: DateTimePicker(controller: fromDate, title: "Từ ngày"),
+              ),
+              SizedBox(height: 13 * height / 440),
+              SizedBox(
+                height: 50 * height / 956,
+                child: DateTimePicker(controller: toDate, title: "Đến ngày"),
+              ),
+              SizedBox(height: 13 * height / 440),
+              ButtonAdmin(onPressed: () {}, title: "Tạo báo cáo"),
+            ],
           ),
         ),
       ),
