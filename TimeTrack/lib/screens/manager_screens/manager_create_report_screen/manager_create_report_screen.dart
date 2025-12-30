d1c185a132bb5d5e26c3235f43cdd4a1831565de
Nguyễn Minh Thông <minhthongnt2610@gmail.com>
2025-12-30 23:11:44 +0700

    hiển thị được thông tin của các nhân viên trong phòng ban

---
+++ TimeTrack/lib/screens/manager_screens/manager_create_report_screen/manager_create_report_screen.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:timetrack/common_widget/button_admin.dart';
+import 'package:timetrack/data/remote/firebase/excelService.dart';
+import 'package:timetrack/data/remote/firebase/firestore_service.dart';
+import 'package:timetrack/models/firebase/fb_bao_cao_phong_ban_model.dart';
+import 'package:timetrack/screens/common_screens/document_screen/widgets/date_time_picker.dart';
+
+import '../../../contains/app_colors.dart';
+
+class ManagerCreateReportScreen extends StatefulWidget {
+  const ManagerCreateReportScreen({super.key, required this.phongBanID});
+
+  final String phongBanID;
+
+  @override
+  State<ManagerCreateReportScreen> createState() =>
+      _ManagerCreateReportScreenState();
+}
+
+class _ManagerCreateReportScreenState extends State<ManagerCreateReportScreen> {
+  final TextEditingController fromDate = TextEditingController();
+  final TextEditingController toDate = TextEditingController();
+  final FirestoreService _store = FirestoreService();
+  bool _loading = false;
+  List<FbBaoCaoPhongBanModel> _baoCao = [];
+  final ExcelService excelService = ExcelService();
+
+  @override
+  Widget build(BuildContext context) {
+    int height = MediaQuery.of(context).size.height.toInt();
+    int width = MediaQuery.of(context).size.width.toInt();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor,
+        centerTitle: true,
+        title: const Text(
+          "QUẢN LÝ TẠO BÁO CÁO",
+          style: TextStyle(
+            color: AppColors.backgroundAppBar,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'balooPaaji',
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50 * height / 956,
+              child: DateTimePicker(controller: fromDate, title: "Từ ngày"),
+            ),
+            const SizedBox(height: 12),
+            SizedBox(
+              height: 50 * height / 956,
+              child: DateTimePicker(controller: toDate, title: "Đến ngày"),
+            ),
+            const SizedBox(height: 16),
+            ButtonAdmin(
+              title: "Tạo báo cáo",
+              onPressed: () async {
+                if (fromDate.text.isEmpty || toDate.text.isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        "Vui lòng chọn đủ Từ ngày và Đến ngày",
+                        style: TextStyle(fontFamily: 'balooPaaji'),
+                      ),
+                    ),
+                  );
+                  return;
+                }
+
+                if (fromDate.text.compareTo(toDate.text) > 0) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        "Từ ngày phải nhỏ hơn Đến ngày",
+                        style: TextStyle(fontFamily: 'balooPaaji'),
+                      ),
+                    ),
+                  );
+                  return;
+                }
+
+                setState(() {
+                  _loading = true;
+                  _baoCao.clear();
+                });
+
+                final data = await _store.quanLyBaoCaoPhongBan(
+                  phongBanId: widget.phongBanID,
+                  tuNgay: fromDate.text,
+                  denNgay: toDate.text,
+                );
+
+                setState(() {
+                  _baoCao = data;
+                  _loading = false;
+                });
+              },
+            ),
+
+            const SizedBox(height: 16),
+            if (_loading)
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: CircularProgressIndicator(),
+              ),
+            if (_baoCao.isNotEmpty)
+              Container(
+                color: AppColors.backgroundColor,
+                width: 420 * width / 440,
+                height: 470 * height / 956,
+                child: ListView.builder(
+                  itemCount: _baoCao.length,
+                  itemBuilder: (_, index) {
+                    final item = _baoCao[index];
+                    return Card(
+                      margin: const EdgeInsets.symmetric(vertical: 6),
+                      child: ListTile(
+                        leading: const Icon(Icons.person_2_outlined),
+                        title: Text(
+                          item.hoTen,
+                          style: TextStyle(fontFamily: 'balooPaaji'),
+                        ),
+                        subtitle: Text(
+                          "Mã: ${item.maNV} | "
+                          "Ngày làm: ${item.soNgayLam} | "
+                          "Ngày nghỉ: ${item.soNgayNghi}",
+                          style: TextStyle(fontFamily: 'balooPaaji'),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ButtonAdmin(
+              onPressed: () async {
+                final file = await excelService.exportBaoCaoPhongBan(_baoCao);
+
+                // Share.shareXFiles([XFile(file.path)]);
+                debugPrint(file.path);
+                if (!mounted) return;
+                await OpenFilex.open(file.path);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      "Đã xuất file Excel:\n${file.path}",
+                      style: TextStyle(fontFamily: 'balooPaaji'),
+                    ),
+                  ),
+                );
+              },
+              title: "Xuất Excel",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
